08e922f20d2c76663d632a37b174cf14c8cd4ca0
Vilner César <vilnercesar@gmail.com>
2023-01-18 20:48:11 -0300

    Usando a função render com context e template

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'recipes/home.html', context={
+        'nome': 'Vilner César'
+    })
 
 
 def sobre(request):
-    return HttpResponse('SOBRE')
+    return render(request, 'me-apague/temp.html')
 
 
 def contato(request):
