02fa5b6c8348f2ddfe7dcac26aca6f460610b6cd
Vilner César <vilnercesar@gmail.com>
2023-01-19 13:36:53 -0300

    css do header e seus filhos concluída

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 # from django.shortcuts import render
@@ -6,6 +6,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'nome': 'Vilner César'
     })
