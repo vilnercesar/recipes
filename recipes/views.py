f34324cbd9266b6331969deef8fbe068540b7c49
Vilner César <vilnercesar@gmail.com>
2023-01-18 21:00:47 -0300

    rm da temporária e adicionado contato.html

--- recipes/views.py
+++ recipes/views.py
@@ -12,8 +12,8 @@ def home(request):
 
 
 def sobre(request):
-    return render(request, 'me-apague/temp.html')
+    return HttpResponse('SOBRE')
 
 
 def contato(request):
-    return HttpResponse('CONTATO')
+    return render(request, 'recipes/contato.html')
