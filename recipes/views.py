542f32e5480a47690dc2fe3e442e67f67506470a
Vilner César <vilnercesar@gmail.com>
2023-01-20 15:00:53 -0300

    Erro ao carregar arquivo css

--- recipes/views.py
+++ recipes/views.py
@@ -9,3 +9,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'nome': 'Vilner César'
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'nome': 'Vilner César'
+    })
