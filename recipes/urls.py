a0d6923076fe03f7825681118812da4b200690ae
Vilner César <vilnercesar@gmail.com>
2023-01-24 15:10:06 -0300

    Ocultando receitas que não estão publicadas

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,8 +7,9 @@ app_name = 'recipes'
 urlpatterns = [
 
     path('', views.home, name='home'),
+    path('recipes/category/<int:category_id>/',
+         views.category, name='category'),
     path('recipes/<int:id>/', views.recipe, name='recipes'),
-
 
 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -3,13 +3,29 @@ from django.shortcuts import render
 
 from utils.recipes.factory import make_recipe
 
+from .models import Category, Recipe
+
 # from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
+
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)]
+        'recipes': recipes
+    })
+
+
+def category(request, category_id):
+
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id')
+    category = Category.objects.filter(id=category_id).first()
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'category': category
     })
 
 
